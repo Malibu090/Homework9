a783162a9bc55793d8184ef517f2aac66e676669
Malibu <malibu090@mail.ru>
2023-01-07 23:20:46 +0300

    Числа от N до 1, рекурсией

---
+++ Task64/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int EnterNumber(string str)
+{
+    Console.Write(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+string OutputNaturalNumbers(int n)
+{
+    if (n < 1)
+    {
+        return string.Empty;
+    }
+    else
+    {
+        return $"{n}, " + OutputNaturalNumbers(n - 1);
+
+    }
+}
+
+int n = EnterNumber("Input number N: ");
+Console.WriteLine(OutputNaturalNumbers(n));
